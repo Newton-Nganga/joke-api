49784bae84b5aae195cdfaedbf52aec0602e86f3
Newton-Nganga <nganga7newt@gmail.com>
2023-10-12 21:47:54 +0300

    refactored code

--- joke.js
+++ joke.js
@@ -2,24 +2,27 @@ const app = require("express")();
 const axios = require("axios");
 
 const PORT = 3000 || process.env.PORT;
+const JOKE_API_BASE_URL = "https://v2.jokeapi.dev/joke/Any";
 
 app.get("/jokes", async (req, res) => {
   try {
     const { keyword, amount } = req.query;
 
-    const jokeCount = amount && amount > 0 && amount <= 3 ? amount : 1;
+    // Ensure jokeCount is within 1 to 3 range
+    const jokeCount = Math.min(Math.max(parseInt(amount, 10) || 1, 1), 3); 
+   
+    const queryParams = {
+      format: "json",
+      amount: jokeCount,
+    };
+  
+    if (keyword && typeof keyword === "string" && keyword.length > 0) {
+      queryParams.contains = keyword; // Add keyword query parameter if provided
+    }
+    
+    // Make a request to Joke API
+    const response = await axios.get(JOKE_API_BASE_URL, { params: queryParams }); 
 
-    const searchKeyword =
-      typeof keyword === "string" && keyword.length > 0 ? keyword : null;
-
-    const queryString = searchKeyword
-      ? `contains=${keyword}&amount=${jokeCount}`
-      : `amount=${jokeCount}`;
-
-    const url = `https://v2.jokeapi.dev/joke/Any?format=json&${queryString}`;
-
-    const response = await axios.get(url);
-    console.log(response)
     if (!response.data) {
       return res.status(404).json({ message: "ERROR: No jokes Found" });
     }
@@ -44,6 +47,7 @@ app.get("/jokes", async (req, res) => {
       message: "SUCCESS: successfully fetched joke(s)",
       data: jokesResponse,
     });
+
   } catch (error) {
     res.status(500).json({
       message: "ERROR:Error caught while trying to get joke(s)",
